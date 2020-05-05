76c6f723f88b96363cdcb40bf0458d2babc781c6
Jamie Peters <=>
2020-05-05 16:26:43 +0100

    wip

--- src/PageViewBuilderServiceProvider.php
+++ src/PageViewBuilderServiceProvider.php
@@ -10,13 +10,13 @@ class PageViewBuilderServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../../views', 'page-view-builder');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'page-view-builder');
     }
 
     public function register()
     {
         if (!$this->app->configurationIsCached()) {
-            $this->mergeConfigFrom(__DIR__ . '/../../config/laravel-page-view.php', 'laravel-page-view');
+            $this->mergeConfigFrom(__DIR__ . '/../config/laravel-page-view.php', 'laravel-page-view');
         }
 
         $instance = new Page($this->app->make(Request::class), $this->app->make(ResponseFactory::class));
@@ -24,6 +24,6 @@ class PageViewBuilderServiceProvider extends ServiceProvider
         $this->app->instance(AbstractPage::class, $instance);
         $this->app->instance(AbstractPage::class, $instance);
 
-        $this->publishes([__DIR__ . '/../../public' => public_path('vendor/page-view-builder')]);
+        $this->publishes([__DIR__ . '/../public' => public_path('vendor/page-view-builder')]);
     }
 }
